2e69f935612021bccb536489c16674f407b6f6bf
Subhash Sarangi <subhashsarangi123@gmail.com>
2022-04-25 16:52:40 +0530

    Added sample questions in an accordion menu. Fixes a subtask of #1

--- src/components/SampleQuestions/Index.jsx
+++ src/components/SampleQuestions/Index.jsx
@@ -1,4 +1,13 @@
-import { Box, Card, CardContent, Paper, Typography } from '@mui/material';
+import {
+	Accordion,
+	AccordionDetails,
+	AccordionSummary,
+	Box,
+	Card,
+	CardContent,
+	Paper,
+	Typography
+} from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { fetchRandomQuestions } from '../../utils/helpers';
 
@@ -8,30 +17,34 @@ const styles = {
 	padding: '15px',
 	fontFamily: 'Cascadia Code',
 	fontSize: '7px 14px',
-    boxShadow: "inset 1px 1px 6px gray",
-    maxWidth: 600,
-    width: 600,
+	boxShadow: 'inset 1px 1px 6px gray',
+	maxWidth: 600,
+	width: 600
 };
 
 export default function SampleQuestions({ task, imageIndex }) {
 	const [ sampleQuestions, setSampleQuestions ] = useState([]);
 
-	useEffect(() => {
-		fetchRandomQuestions(imageIndex, task).then(setSampleQuestions);
-	}, [imageIndex]);
+	useEffect(
+		() => {
+			fetchRandomQuestions(imageIndex, task).then(setSampleQuestions);
+		},
+		[ imageIndex ]
+	);
 
 	return (
-		<Card sx={styles}>
-			<CardContent sx={{ color: '#2345b0', fontWeight: 'bold', textAlign: 'left' }}>
-				<Typography gutterBotton variant="h6" component="div">
-					Sample Questions
-				</Typography>
-			</CardContent>
-			<Box sx={{ color: '#118834' }}>
-				{sampleQuestions.map((q) => {
-					return <div key={q}>{q}</div>;
-				})}
-			</Box>
-		</Card>
+		<Box sx={styles}>
+			<Typography variant="h6" sx={{fontFamily: "Cascadia Code"}}>Sample Questions</Typography>
+			{sampleQuestions.map((q) => {
+				return (
+					<Accordion>
+						<AccordionSummary sx={{ color: '#118834' }}>{q}</AccordionSummary>
+						<AccordionDetails sx={{ color: '#2345b0', fontWeight: 'bold', textAlign: 'left' }}>
+							Answer
+						</AccordionDetails>
+					</Accordion>
+				);
+			})}
+		</Box>
 	);
 }
