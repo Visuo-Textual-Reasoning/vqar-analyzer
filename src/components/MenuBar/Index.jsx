2ebc2672c546c57fe1deaba37d5cff389232eb27
Subhash Sarangi <subhashsarangi123@gmail.com>
2022-05-04 10:06:34 +0530

    Fixed a minor issue with the appbar. Resolves a subtask of #1

--- src/components/MenuBar/Index.jsx
+++ src/components/MenuBar/Index.jsx
@@ -23,12 +23,17 @@ export default function MenuBar() {
 		<Box sx={{ flexGrow: 1 }}>
 			<AppBar position="static">
 				<Toolbar>
-					<IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-						<MenuIcon
-                onClick={toggleDrawer}
-             />
+					<IconButton
+						size="large"
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						sx={{ mr: 2 }}
+						onClick={toggleDrawer}
+					>
+						<MenuIcon />
 					</IconButton>
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center'}}>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
 						VQAR Analyzer
 					</Typography>
 					<Button color="inherit">Login</Button>
