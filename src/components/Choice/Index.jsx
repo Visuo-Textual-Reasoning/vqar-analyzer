615d7b07441f69fc17572c4cea3571b816ad7cc9
Subhash Sarangi <subhashsarangi123@gmail.com>
2022-05-04 11:57:44 +0530

    Added a choice component for vcr

---
+++ src/components/Choice/Index.jsx
@@ -0,0 +1,64 @@
+import { Box, Button, TextField } from '@mui/material';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@mui/styles';
+import React from 'react';
+import { useEvaluate } from '../../contexts/EvaluateProvider';
+
+const useStyles = makeStyles((theme) => {
+	return {
+		box: {
+			width: 'fit-content',
+			margin: '0 auto',
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			marginTop: 8,
+			// border: "1px solid red",
+
+			'& input': {
+				width: 530,
+				height: 20,
+				fontFamily: 'Cascadia Code',
+				fontSize: '14px',
+				marginLeft: '15px',
+				[theme.breakpoints.down('md')]: {
+					width: 400
+				},
+				[theme.breakpoints.down('sm')]: {
+					width: 300
+				}
+			},
+			'& button': {
+				height: 40,
+				fontFamily: 'Cascadia Code',
+				[theme.breakpoints.down('sm')]: {
+					height: 30
+				}
+			}
+		}
+	};
+});
+
+export default function Choice({ value, valueChangeHandler, label }) {
+	const classes = useStyles();
+
+	return (
+		<Box className={classes.box}>
+			<TextField
+				id="filled-basic"
+				label={label}
+				color="primary"
+				variant="standard"
+				value={value}
+				onChange={valueChangeHandler}
+				focused
+			/>
+		</Box>
+	);
+}
+
+Choice.propTypes = {
+	value: PropTypes.string.isRequired,
+	valueChangeHandler: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+};
