f8b68d26ae866fecabd49d8a14cbf0ee62cbccb4
parikshitrao <55613484+parikshitrao@users.noreply.github.com>
2022-12-02 11:20:47 +0530

    add port no to react script

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -1,10 +1,10 @@
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import VQAImage from '../../assets/images/vqa-example.png';
 import VCRImage from '../../assets/images/vcr-example.png';
